b1a14f9268341aec14bdd9e7ef7bee889fda0543
Fariza6 <homidovafariza536@gmail.com>
2021-01-09 12:46:42 +0300

    полоски

---
+++ turtle/полоски.py
@@ -0,0 +1,24 @@
+from turtle import*
+
+width(15)
+
+color('orange')
+forward(90)
+
+up()
+left(90)
+forward(30)
+left(90)
+down()
+
+width(15)
+
+color('blue')
+forward(90)
+
+
+
+
+
+exitonclick()
+
--- turtle/самостоятельная работа.py
+++ turtle/самостоятельная работа.py
@@ -1,3 +1,4 @@
+
 from turtle import*
 
 width(10)
