749a85c95a21e649f77e8df7dbb8c69386cd3129
Fariza6 <homidovafariza536@gmail.com>
2021-01-09 16:22:07 +0300

    Fariza

---
+++ turtle/redsquare.py
@@ -0,0 +1,19 @@
+from  turtle import*
+
+color('red')
+fillcolor('red')
+
+begin_fill()
+
+left(50)
+forward(120)
+left(90)
+forward(120)
+left(90)
+forward(120)
+left(90)
+forward(120)
+
+end_fill()
+
+exitonclick()
