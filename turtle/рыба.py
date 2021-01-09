2c2236607f4d6d36b507a29a0e767313eb5888a2
Fariza6 <homidovafariza536@gmail.com>
2021-01-09 16:56:16 +0300

    Fish

---
+++ turtle/рыба.py
@@ -0,0 +1,25 @@
+from turtle import*
+
+width(5)
+color('green')
+fillcolor('green')
+
+left(90)
+forward(100)
+right(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+
+width(5)
+color('red')
+fillcolor('red')
+
+forward(100)
+left(90)
+forward(100)
+right(90)
+forward(100)
+
+exitonclick()
