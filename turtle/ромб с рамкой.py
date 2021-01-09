dadbeb9c724666745664143372d6b3def6981dc7
Fariza6 <homidovafariza536@gmail.com>
2021-01-09 20:41:37 +0300

    rhombus

---
+++ turtle/ромб с рамкой.py
@@ -0,0 +1,20 @@
+from  turtle import*
+
+width(15)
+color('green')
+fillcolor('red')
+
+begin_fill()
+
+left(50)
+forward(120)
+left(90)
+forward(120)
+left(90)
+forward(120)
+left(90)
+forward(120)
+
+end_fill()
+
+exitonclick()
